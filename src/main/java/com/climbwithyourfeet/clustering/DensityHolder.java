59eb69acab46e2c8edc11d351394bc1032fa5b8f
nichole <nichole@climbwithyourfeet.com>
2017-06-28 11:34:48 -0700

    adding javadoc comment on class contents

--- src/main/java/com/climbwithyourfeet/clustering/DensityHolder.java
+++ src/main/java/com/climbwithyourfeet/clustering/DensityHolder.java
@@ -1,17 +1,28 @@
 package com.climbwithyourfeet.clustering;
 
 /**
- *
+ * contains the surface densities and their frequencies and a representation
+ * of the PMF or PDF as the arrays (dens, normCount).
+ * 
  * @author nichole
  */
 public class DensityHolder {
     
     public float critDens = Float.POSITIVE_INFINITY;
     
+    /**
+     * surface densities
+     */
     public float[] dens = null;
     
+    /**
+     * frequency of the surface densities
+     */
     public float[] count = null;
     
+    /**
+     * the count, normalized to sum to 1.
+     */
     public float[] normCount = null;
     
 }
