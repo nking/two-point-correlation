59154f5eb3402144325a574b82b625d9d7c38a07
nichole <nichole@climbwithyourfeet.com>
2017-07-03 21:34:23 -0700

    alternate implementation of critical separation in progress.
    Signed-off-by: nichole <nichole@climbwithyourfeet.com>

--- src/main/java/com/climbwithyourfeet/clustering/PairwiseSeparations.java
+++ src/main/java/com/climbwithyourfeet/clustering/PairwiseSeparations.java
@@ -1,5 +1,7 @@
 package com.climbwithyourfeet.clustering;
 
+import algorithms.VeryLongBitString;
+import algorithms.connected.ConnectedValuesGroupFinder;
 import algorithms.imageProcessing.FFTUtil;
 import algorithms.imageProcessing.Filters;
 import algorithms.misc.Complex;
@@ -9,6 +11,10 @@ import algorithms.util.PixelHelper;
 import gnu.trove.iterator.TIntIterator;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 import java.awt.image.BufferedImage;
@@ -16,6 +22,7 @@ import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -33,8 +40,8 @@ public class PairwiseSeparations {
     public void setToDebug() {
         debug = true;
     }
-    
-    private static class ScaledPoints {
+
+    public static class ScaledPoints {
         public TIntSet pixelIdxs;
         int width;
         int height;
@@ -49,18 +56,15 @@ public class PairwiseSeparations {
                 "pixelIdxs.size must be 12 or more");
         }
         
-        ScaledPoints sp = scaleThePoints(pixelIdxs, width, height);
-        
         // these are the non-point distances to the points        
-        int[][] dt = DistanceTransformUtil.transform(sp.pixelIdxs, sp.width, sp.height);
+        int[][] dt = DistanceTransformUtil.transform(pixelIdxs, width, height);
         
         for (int i = 0; i < dt.length; ++i) {
             for (int j = 0; j < dt[i].length; ++j) {
                 int d = dt[i][j];
                 if (d > 0) {
                     if (d > 2) {
-                        d = (int)Math.round(Math.sqrt(d));
-                        dt[i][j] = d;
+                        dt[i][j] = (int)Math.round(Math.sqrt(d));
                     }
                 }
             }
@@ -78,22 +82,73 @@ public class PairwiseSeparations {
         
            then will find which groups, that is values, are the maxima.
         
-           the critical separation is the largest of the maxima, but will
+           the critical separation is the smallest of the maxima, but will
                want to look at the counts of that where possible.
         
         caveats:
-            single group of points in center will have no maxima found as 
-                described unless there are gaps in points in the cluster.
-                if there are gaps,
-                    that will be found.
-                else the critical separation will be found to be 1
+           single group of points in center will have no maxima found as 
+               described unless there are gaps in points in the cluster.
+               if there are gaps,
+                  that will be found.
+               else the critical separation will be found to be 1
         
         Note that throughout the refactoring in code:
             -- the factor of 2 is dropped here for 2 points within distance
-            -- the facotr of 2 should be dropped in the group finder
+            -- the factor of 2 should be dropped in the group finder
             -- a factor of 1/thresholdFactor should possibly be applied 
                here for the caveat case, for example
         */
+                
+        ConnectedValuesGroupFinder finder = new ConnectedValuesGroupFinder();
+        List<TIntSet> valueGroups = finder.findGroups(dt);
+        
+        // key = index of values Group, value = adjacent indexes of valuesGroup
+        TIntObjectMap<VeryLongBitString> adjMap = createAdjacencyMap(
+            valueGroups, width, height);
+            
+        // search for valueGroups which has value larger than all neighbors
+        TIntSet groupMaximaIdxs = new TIntHashSet();
+        
+        PixelHelper ph = new PixelHelper();
+        int[] xy = new int[2];
+        
+        for (int i = 0; i < valueGroups.size(); ++i) {
+            
+            if (!adjMap.containsKey(i)) {
+                continue;
+            }
+            
+            TIntSet group = valueGroups.get(i);
+            
+            int tPix = group.iterator().next();
+            ph.toPixelCoords(tPix, width, xy);
+            int v = dt[xy[0]][xy[1]];
+            
+            boolean allAreLower = true;
+            
+            VeryLongBitString adj = adjMap.get(i);
+            
+            int[] adjGroupIdxs = adj.getSetBits();
+            for (int aIdx : adjGroupIdxs) {
+                TIntSet group2 = valueGroups.get(aIdx);
+                int tPix2 = group2.iterator().next();
+                ph.toPixelCoords(tPix2, width, xy);
+                int v2 = dt[xy[0]][xy[1]];
+            
+                if (v2 > v) {
+                    allAreLower = false;
+                    break;
+                }
+            }
+            
+            if (allAreLower) {
+                groupMaximaIdxs.add(i);
+            }
+        }
+        
+        // look at frequency of groupMaximaIdxs values
+        // 
+        
         
         /*
         Frequency f = new Frequency();
@@ -169,6 +224,69 @@ public class PairwiseSeparations {
         sp.yScale = xyScales[1];
     
         return sp;
+    }
+    
+    private TIntObjectMap<VeryLongBitString> createAdjacencyMap(
+        List<TIntSet> groupList, int width, int height) {
+        
+        TIntIntMap pixGroupMap = new TIntIntHashMap();
+        for (int i = 0; i < groupList.size(); ++i) {
+            TIntSet group = groupList.get(i);
+            TIntIterator iter = group.iterator();
+            while (iter.hasNext()) {
+                int pixIdx = iter.next();
+                assert(!pixGroupMap.containsKey(pixIdx));
+                pixGroupMap.put(pixIdx, i);
+            }
+        }
+        
+        PixelHelper ph = new PixelHelper();
+        int[] dx8 = new int[]{-1, -1,  0,  1, 1, 1, 0, -1};
+        int[] dy8 = new int[]{ 0, -1, -1, -1, 0, 1, 1,  1};
+        
+        int[] xy = new int[2];
+        
+        int nBSLen = groupList.size();
+        
+        TIntObjectMap<VeryLongBitString> adjMap 
+            = new TIntObjectHashMap<VeryLongBitString>();
+        
+        for (int i = 0; i < groupList.size(); ++i) {
+            TIntSet group = groupList.get(i);
+            TIntIterator iter = group.iterator();
+            while (iter.hasNext()) {
+                int pixIdx = iter.next();
+                ph.toPixelCoords(pixIdx, width, xy);
+                
+                for (int k = 0; k < dx8.length; ++k) {
+                    int vX = xy[0] + dx8[k];
+                    int vY = xy[1] + dy8[k];
+                    if (vX < 0 || vY < 0 || vX >= width || vY >= height) {
+                        continue;
+                    }
+                    int pixIdx2 = ph.toPixelIndex(vX, vY, width);
+                    
+                    if (!pixGroupMap.containsKey(pixIdx2)) {
+                        continue;
+                    }
+                    
+                    int j = pixGroupMap.get(pixIdx2);
+                    
+                    if (i == j) {
+                        continue;
+                    }
+                    
+                    VeryLongBitString adj = adjMap.get(i);
+                    if (adj == null) {
+                        adj = new VeryLongBitString(nBSLen);
+                        adjMap.put(i, adj);
+                    }
+                    adj.setBit(j);
+                }
+            }
+        }
+        
+        return adjMap;
     }
     
      private void writeDebugImage(double[][] dt, String fileSuffix, int width, 
