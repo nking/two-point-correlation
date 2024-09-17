3afd35c4b2347c5c0e7a35908f62877a662a1eef
nking <nichole@climbwithyourfeet.com>
2024-09-17 16:51:10 -0700

    updating javadocs

--- docs/api/package-search-index.js
+++ docs/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.climbwithyourfeet.clustering"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.climbwithyourfeet.clustering"}];updateSearchResults();
